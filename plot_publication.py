601d25d1157b5941afa51f587f8269fce281c470
Ruozhou LIN <ruozhou.lin@my.cityu.edu.hk>
2024-04-08 11:53:38 +0800

    Add unit and improve figure sizing

--- plot_publication.py
+++ plot_publication.py
@@ -12,6 +12,7 @@ quality figures for publication
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import matplotlib
 
 # * ====================================================================
@@ -22,21 +23,68 @@ MEDIUM_SIZE = 10
 BIGGER_SIZE = 12
 
 # controls default text sizes
-font = {'family': 'Times New Roman',
-        'weight': 'normal',  # bold
-        'size': SMALL_SIZE}
-matplotlib.rc('font', **font)
+font = {"family": "Times New Roman", "weight": "normal", "size": SMALL_SIZE}  # bold
+matplotlib.rc("font", **font)
 # fontsize of the axes title, namely title of subplot
-plt.rc('axes', titlesize=MEDIUM_SIZE)
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc("axes", titlesize=MEDIUM_SIZE)
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 # * =====================================================================
+# set latex font
+matplotlib.rcParams["mathtext.fontset"] = "cm"
+matplotlib.rcParams["mathtext.rm"] = "serif"
 
 
-class Page():
+class UnitTex:
+    # https://en.wikipedia.org/wiki/International_System_of_Units
+
+    length_options = ("mm", "cm", "m", "km")
+    time_options = ("s", "min", "h", "d")
+    mass_options = ("mg", "g", "kg")
+
+    @classmethod
+    def time(cls, time: str) -> str:
+        assert time in cls.time_options
+
+        unit_time = "%s" % time
+        return "$\mathrm{%s}$" % (unit_time)
+
+    @classmethod
+    def area(cls, length: str) -> str:
+        assert length in cls.length_options
+
+        unit_length = "%s" % length
+        return "$\mathrm{%s}$" % (unit_length)
+
+    @classmethod
+    def velocity(cls, length: str, time: str) -> str:
+        assert length in cls.length_options
+        assert time in cls.time_options
+
+        unit_length = "%s" % length
+        unit_time = "%s" % time
+        return "$\mathrm{%s/%s}$" % (unit_length, unit_time)
+
+    @classmethod
+    def discharge(cls, length: str, time: str) -> str:
+        assert length in cls.length_options
+        assert time in cls.time_options
+
+        unit_length = "%s" % length
+        unit_time = "%s" % time
+        return "$\mathrm{%s^{3}/%s}$" % (unit_length, unit_time)
+
+    @classmethod
+    def show_options(cls) -> None:
+        print("Length:\t", cls.length_options)
+        print("Time:\t", cls.time_options)
+        print("Mass:\t", cls.mass_options)
+
+
+class Page:
     """This class define the size of the page/slide that you want to insert your figure.
     Please set the `height`, `width`, and `margin` of the page/slide in inch.
 
@@ -52,7 +100,7 @@ class Page():
         self.validate()  # Check if the above parameters are reasonable.
 
     def validate(self):
-        """Check if page parameters are reasonable, i.e., the margin should be 
+        """Check if page parameters are reasonable, i.e., the margin should be
         positive and should not be larger than the page.
         """
         assert self.__height > 0 and self.__width > 0
@@ -73,8 +121,8 @@ class Page():
 
     @property
     def body_size(self):
-        height = self.__height-self.__margin[0]-self.__margin[1]
-        width = self.__width-self.__margin[2]-self.__margin[3]
+        height = self.__height - self.__margin[0] - self.__margin[1]
+        width = self.__width - self.__margin[2] - self.__margin[3]
         return height, width
 
     @property
@@ -87,67 +135,66 @@ class Page():
         self.validate()  # Check if the above parameters are reasonable.
 
     def print_page_setting(self):
-        print('The size of the page is %.2f * %.2f.' %
-              (self.__height, self.__width))
-        print("""The margin is:
+        print("The size of the page is %.2f * %.2f." % (self.__height, self.__width))
+        print(
+            """The margin is:
     Top:    %.2f
     Bottom: %.2f
     Left:   %.2f
-    Right:  %.2f""" % self.__margin)
+    Right:  %.2f"""
+            % self.__margin
+        )
 
     # set space between subplot
     def set_width_space(self, v=0.25):
-        plt.rcParams['figure.subplot.wspace'] = v
+        plt.rcParams["figure.subplot.wspace"] = v
 
     def set_height_space(self, v=0.3):
-        plt.rcParams['figure.subplot.hspace'] = v
+        plt.rcParams["figure.subplot.hspace"] = v
 
 
 class PageA4(Page):
-    """Create a subclass of `Page` that has a size of A4 and use default margin setting in Word.
-    """
+    """Create a subclass of `Page` that has a size of A4 and use default margin setting in Word."""
 
     def __init__(self, height=11.69, width=8.27, margin=(1, 1, 1.25, 1.25)) -> None:
         super().__init__(height, width, margin)
 
     def print_page_setting(self):
-        print('Page A4:')
+        print("Page A4:")
         super().print_page_setting()
 
 
 class PageLetter(Page):
-    """Create a subclass of `Page` that has a size of Letter and use default margin setting in Word.
-    """
+    """Create a subclass of `Page` that has a size of Letter and use default margin setting in Word."""
 
     def __init__(self, height=11, width=8.5, margin=(1, 1, 1.25, 1.25)) -> None:
         super().__init__(height, width, margin)
 
     def print_page_setting(self):
-        print('Page Letter:')
+        print("Page Letter:")
         super().print_page_setting()
 
 
 class PageSlide(Page):
-    """Create a subclass of `Page` that has a size of slide and use default setting in PowerPoint.
-    """
+    """Create a subclass of `Page` that has a size of slide and use default setting in PowerPoint."""
 
-    def __init__(self, height=0, width=0, aspectRatio='4:3', margin=(0, 0, 0, 0)) -> None:
-        if aspectRatio == '4:3':
+    def __init__(self, height=0, width=0, aspectRatio="4:3", margin=(0, 0, 0, 0)) -> None:
+        if aspectRatio == "4:3":
             height, width = 7.5, 10
-        elif aspectRatio == '16:9':
+        elif aspectRatio == "16:9":
             height, width = 7.5, 13.33
         else:
             pass
         super().__init__(height, width, margin)
 
     def print_page_setting(self):
-        print('Page for slides:')
+        print("Page for slides:")
         super().print_page_setting()
 
 
-class FigurePublication():
+class FigurePublication:
     def __init__(self, nrows, ncols, page=PageA4(), xrate=None, yrate=None, tightLayout=True) -> None:
-        self.__bbox_inches = 'tight'
+        self.__bbox_inches = "tight"
         # 300 is usually minimum requirement for high resolution images, 600 is better
         self.__dpi = 300
         self.page: Page = page
@@ -157,21 +204,31 @@ class FigurePublication():
         self.tightLayout = tightLayout
         self.arrange(xrate, yrate)
 
-    def update(self, tightLayout=True):
-        if tightLayout:  # auto adjust layout
-            self.fig.tight_layout()
+        # make sure the height of each sub-figure equals the width
+        if ncols > 1:
+            for ax in self.ax.flatten():  # convert to 1d array
+                ax.set_box_aspect(1)
 
     def arrange(self, xrate=None, yrate=None):
         """Automatically set size of the figure according to the page size and figure content.
         You can also do this manually by setting `xrate` and `yrate`.
         """
         # this rate is for sub-figure with full x-y labels
-        xy = (((0.6, 0.30),  (1.0, 0.28), (1.0, 0.25), (1.0, 0.25)),
-              ((1.0, 0.55),  (1.0, 0.55), (1.0, 0.55), (1.0, 0.55)),
-              ((1.0, 0.80),  (1.0, 0.80), (1.0, 0.80), (1.0, 0.80)),
-              ((1.0, 1.00),  (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)))
-
-        xrate1, yrate1 = xy[self.__nrows-1][self.__ncols-1]
+        # xy = (
+        #     ((0.6, 0.30), (1.0, 0.28), (1.0, 0.25), (1.0, 0.25)),
+        #     ((1.0, 0.55), (1.0, 0.55), (1.0, 0.55), (1.0, 0.55)),
+        #     ((1.0, 0.80), (1.0, 0.80), (1.0, 0.80), (1.0, 0.80)),
+        #     ((1.0, 1.00), (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)),
+        # )
+        # * only need to define width because height will be automatically adjusted by `ax.set_box_aspect(1)`
+        xy = (
+            ((0.6, 0.30), (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)),
+            ((1.0, 0.55), (1.0, 0.55), (1.0, 0.55), (1.0, 0.55)),
+            # ((1.0, 1.00), (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)),
+            ((1.0, 1.00), (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)),
+            ((1.0, 1.00), (1.0, 1.00), (1.0, 1.00), (1.0, 1.00)),
+        )
+        xrate1, yrate1 = xy[self.__nrows - 1][self.__ncols - 1]
         # Check these parameters are set manully.
         if xrate is not None:
             xrate1 = xrate
@@ -182,39 +239,42 @@ class FigurePublication():
         width1 = width * xrate1
         height1 = height * yrate1
         self.fig.set_size_inches(width1, height1)
-        # auto adjust layout
-        self.update()
 
     def save(self, savename: str, bbox_inches=None, dpi=None):
-        """Save the figure in .svg format.
-        """
+        """Save the figure in .svg format."""
         # format of save name should be "directory/figure.svg"
-        assert savename.split('.')[-1] == 'svg'
+        format = savename.split(".")[-1]
+        assert format == "svg" or format == "png"
+
         # if dpi is not assigned external, use dpi store in this class
         if dpi is None:
             dpi = self.__dpi
-        # same for bbox_inces
-        if bbox_inches is None:
-            bbox_inches = self.__bbox_inches
-        self.update()
-        self.fig.savefig(savename, format='svg',
-                         bbox_inches=bbox_inches,
-                         dpi=dpi)
+
+        # Warning: only the given portion of the figure is saved
+        # This will change size of the figure
+        # if bbox_inches is None:
+        #     bbox_inches = self.__bbox_inches
+        # self.fig.savefig(savename, bbox_inches=bbox_inches, dpi=dpi)
+
+        self.fig.savefig(savename, dpi=dpi)
 
     def change_page(self, newpage=PageSlide()):
         # modify paper size, such as from A4 to a slide in 16:9
         self.page = newpage
         self.arrange()
 
+    def stretch_figure_height(self, ratio: float) -> None:
+        width, height = self.fig.get_size_inches()
+        self.fig.set_size_inches(width, height * ratio)
 
-def get_default_color(type='rgb') -> list:
+
+def get_default_color(type="rgb") -> list:
     # [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd',
     # u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
-    lst = matplotlib.rcParams['axes.prop_cycle'].by_key()['color']  # hex
-    if type == 'rgb':
+    lst = matplotlib.rcParams["axes.prop_cycle"].by_key()["color"]  # hex
+    if type == "rgb":
         # convert hex to rgb that ranges from 0 to 1
-        lst = [list(int(h.lstrip('#')[i:i+2], 16)/256
-                    for i in (0, 2, 4)) for h in lst]
+        lst = [list(int(h.lstrip("#")[i : i + 2], 16) / 256 for i in (0, 2, 4)) for h in lst]
     return lst
 
 
@@ -222,7 +282,17 @@ def legend(ax):
     ax.legend(frameon=False)  # remove legend background
 
 
+def set_tick_number_x(tick_number: int, ax) -> None:
+    loc = MaxNLocator(tick_number)
+    ax.xaxis.set_major_locator(loc)
+
+
+def set_tick_number_y(tick_number: int, ax) -> None:
+    loc = MaxNLocator(tick_number)
+    ax.yaxis.set_major_locator(loc)
+
+
 # * check whether font exist
 if __name__ == "__main__":
-    fontPath = matplotlib.font_manager.findfont('Times New Roman')
+    fontPath = matplotlib.font_manager.findfont("Times New Roman")
     print(fontPath)
